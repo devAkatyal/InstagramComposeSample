df4d0017d4de72e13904fff9846d5f0504839d45
ayush <ayushdxb@hotmail.com>
2024-01-16 17:41:17 +0530

    Added preview for screens

--- app/src/main/java/com/practice/instagramcomposesample/MainActivity.kt
+++ app/src/main/java/com/practice/instagramcomposesample/MainActivity.kt
@@ -4,24 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.practice.instagramcomposesample.destinations.MainScreenDestination
 import com.practice.instagramcomposesample.ui.theme.InstagramComposeSampleTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.manualcomposablecalls.composable
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
@@ -36,7 +47,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Destination(start = true)
+@RootNavGraph(start = true)
+@Destination
 @Composable
 fun SplashScreen(navigator: DestinationsNavigator) {
     Box(
@@ -45,7 +57,10 @@ fun SplashScreen(navigator: DestinationsNavigator) {
     ) {
         Image(
             painter = painterResource(id = R.drawable.instagram_logo),
-            contentDescription = "Splash"
+            contentDescription = "Splash",
+            modifier = Modifier
+                .requiredHeight(58.dp)
+                .fillMaxWidth()
         )
     }
     LaunchedEffect(Unit) {
@@ -62,4 +77,18 @@ fun MainScreen(navigator: DestinationsNavigator) {
         text = "Hello, Main Screen!",
         modifier = Modifier.padding(16.dp)
     )
+}
+
+@Preview
+@Composable
+fun SimpleComposablePreview() {
+    InstagramComposeSampleTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+//            SplashScreen(navigator = EmptyDestinationsNavigator)
+            MainScreen(navigator = EmptyDestinationsNavigator)
+        }
+    }
 }
