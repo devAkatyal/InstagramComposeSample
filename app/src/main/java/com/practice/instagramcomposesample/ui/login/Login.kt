c794406aba89792b15968ece653c4960e323cd97
ayush <ayushdxb@hotmail.com>
2024-01-16 18:50:45 +0530

    Created Login Screen UI with default components

--- app/src/main/java/com/practice/instagramcomposesample/ui/login/Login.kt
+++ app/src/main/java/com/practice/instagramcomposesample/ui/login/Login.kt
@@ -1,12 +1,21 @@
 package com.practice.instagramcomposesample.ui.login
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,9 +37,80 @@ fun LoginScreen(navigator: DestinationsNavigator) {
     ) {
         InstagramLogo()
         Spacer(modifier = Modifier.height(42.dp))
-
+        //todo customize email textfield
+        TextField(
+            value = "",
+            placeholder = { Text("Enter your Email") },
+            onValueChange = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 30.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        //todo customize password textfield
+        TextField(
+            value = "",
+            placeholder = { Text("Enter your Password") },
+            onValueChange = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 30.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        //todo customize button
+        Button(
+            onClick = {
+                //your onclick code here
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 30.dp)
+        ) {
+            Text(text = "LOGIN")
+        }
+        //todo add color and update margins
+        HorizontalLinesWithText()
+        //todo add color and make bold
+        Text(
+            text = "Sign Up with Email",
+            modifier = Modifier
+                .padding(top = 20.dp)
+        )
     }
 }
+
+@Composable
+fun HorizontalLinesWithText() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 30.dp)
+            .padding(top = 45.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Divider(
+            modifier = Modifier
+                .weight(1f)
+                .height(1.dp)
+                .background(MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f))
+        )
+        Text(
+            text = "OR",
+            modifier = Modifier
+                .padding(horizontal = 8.dp),
+            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+        )
+
+        Divider(
+            modifier = Modifier
+                .weight(1f)
+                .height(1.dp)
+                .background(MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f))
+        )
+    }
+}
+
 
 @Preview
 @Composable
